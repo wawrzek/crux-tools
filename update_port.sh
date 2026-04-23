1c7f2060686eb26d5e57f7c929761e04e6613e80
Wawrzyniec 'Wawrzek' Niewodniczański <main@wawrzek.name>
2026-04-23 18:29:16 +0100

    update_port: option to push to git and print help

--- update_port.sh
+++ update_port.sh
@@ -1,13 +1,37 @@
 #!/bin/bash
-# Simple script to automate port update. It requires rootlesskit
-# The script:
-# - downloads source and build the package (ignoring footprint &
-#   signature)
-# - updates footprint (ignoring signature)
-# - updates signature
+
+print_help(){
+
+echo """
+Simple script to automate port update. It requires rootlesskit
+The script:
+  - downloads source and build the package (ignoring footprint &
+    signature)
+  - updates footprint (ignoring signature)
+  - updates signature
+  - optional: create git commit in pushes it to the remote repo
+"""
+exit 0
+}
+
+GIT=0
+
+while getopts gh flag
+do
+	case "${flag}" in
+		g) GIT=1;;
+		h) print_help;;
+	esac
+done
 
 command="rootlesskit pkgmk"
 
 $command -d -if -is && \
- $command -uf -is && \
- $command -us
+$command -uf -is && \
+	$command -us
+
+if [[ $GIT -eq 1 ]]; then
+	git commit -a -m:"$(basename $PWD): update version" && git push
+fi
+
+# vim: set noet ts=4 sw=4:
